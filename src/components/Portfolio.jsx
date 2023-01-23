1691180dbfca3e67215122e40cb768b0ff5d8198
yurigligoric <yurigligoric@gmail.com>
2023-01-23 16:20:09 +0800

    second commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,11 +4,11 @@ import RevUp from '../images/RevUp.png'
 
 const Portfolio = () => {
   return (
-    <div >
+    <div className='bg-gradient-to-r from-cyan-500 to-blue-500'>
         <h2 className='font-bold bg-yellow-300 text-4xl '>Games</h2>
         <div className='flex py-6'>
             <img src={NinjaRunner} alt='/' className='max-w-[400px] max-h-[400px] object-cover ml-16 px-4 ' />
-            <div className='grid ml-5 px-4 py-10'>
+            <div className='bg-gray-200 grid ml-5 px-4 py-10 shadow-lg w-full border rounded-t-full'>
                 <p>Endless Ninja Runner</p>
                 <p>Date: 2019</p>
                 <h3>Description</h3>
